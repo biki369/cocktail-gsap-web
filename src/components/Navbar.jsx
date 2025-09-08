d6cb69718d9476acfc6521fac24e1c4e832476d0
biki <bikitirua321@gmail.com>
2025-09-08 19:53:47 +0530

    8.9.25::18:53

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ const Navbar = () => {
 
     navTween.fromTo("nav", { backgroundColor: 'transparent' },
       {
-        backgroundColor: "#00000050",
-        backgroundFilter: "blur(10px)",
+        backgroundColor: '#00000050',
+        backgroundFilter: 'blur(10px)',
         duration: 1,
         ease: "power1.inOut"
       });
